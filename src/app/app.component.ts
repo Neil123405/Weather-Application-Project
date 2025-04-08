3538b2544e8a7dc5cc7683d5e8229eaa8d67c2e5
Neil <156774573+Neil123405@users.noreply.github.com>
2025-04-08 23:17:17 +0800

    app commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { PushNotifications, Token, PushNotificationSchema, ActionPerformed } from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  standalone: false,
+  standalone: false
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initPushNotifications();
+  }
+
+  initPushNotifications() {
+
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        PushNotifications.register();
+      }
+    });
+
+
+    PushNotifications.addListener('registration', (token: Token) => {
+      console.log('FCM Token:', token.value);alert('FCM Token: ' + token.value);  
+    });
+
+    PushNotifications.addListener('pushNotificationReceived', (notification: PushNotificationSchema) => {
+      console.log('Notification received:', notification);
+    });
+
+
+    PushNotifications.addListener('pushNotificationActionPerformed', (action: ActionPerformed) => {
+      console.log('Notification action performed:', action);
+    });
+  }
 }
